4e9444247c5e9f5a00cdab1170de60b5e39b550c
Luca Fiore <lf44551@gmail.com>
2025-12-15 16:25:49 +0100

    Aggiungi array di oggetti per rappresentare squadre di calcio

--- script.js
+++ script.js
@@ -27,4 +27,24 @@ for (let i = 1; i < bici.length; i++) {
 // Utilizziamo console.log per mostrare il nome e il peso della bici più leggera
 console.log(`La bici con il peso minore è: ${biciLeggera.nome}, con un peso di ${biciLeggera.peso} kg.`);
 
+// Snack2
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+
+// Passo 1: Creare un array di oggetti rappresentanti le squadre di calcio
+// Ogni oggetto avrà le proprietà 'nome', 'puntiFatti' (inizialmente 0), 'falliSubiti' (inizialmente 0)
+const squadre = [
+    { nome: 'Juventus', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Milan', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Inter', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Roma', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Napoli', puntiFatti: 0, falliSubiti: 0 }
+];
+
+
 

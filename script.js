e3ac87c1348d1666c4c139645c3e7bf7a9a0e48a
Luca Fiore <lf44551@gmail.com>
2025-12-15 16:24:06 +0100

    Aggiungi array di oggetti per descrivere bici da corsa

---
+++ script.js
@@ -0,0 +1,14 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const bici = [
+    { nome: 'Bici1', peso: 8.5 },
+    { nome: 'Bici2', peso: 7.2 },
+    { nome: 'Bici3', peso: 9.1 },
+    { nome: 'Bici4', peso: 6.8 },
+    { nome: 'Bici5', peso: 8.0 }
+];
+
+
